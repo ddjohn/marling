6352a3470de0da20b33cb0f09854c34f53d9a146
David Johansson <ddjohn@gmail.com>
2017-02-04 16:17:13 +0100

    sodium

--- main/src/main.cc
+++ main/src/main.cc
@@ -6,9 +6,10 @@
 
 CLASS("");
 
-int main() {
+int main(int args, char* argv[]) {
 	METHOD("main()");
 
+if(args > 1) {
     zmq::context_t context (1);
     zmq::socket_t socket (context, ZMQ_REP);
     socket.bind ("tcp://*:5555");
@@ -25,6 +26,25 @@ int main() {
         memcpy (reply.data (), "World", 5);
         socket.send (reply);
     }
+} else {
+
+
+    zmq::context_t context (1);
+    zmq::socket_t socket (context, ZMQ_REQ);
+    std::cout << "Connecting to hello world server…" << std::endl;
+    socket.connect ("tcp://localhost:5555");
+
+    for (int request_nbr = 0; request_nbr != 10; request_nbr++) {
+        zmq::message_t request (5);
+        memcpy (request.data (), "Hello", 5);
+        std::cout << "Sending Hello " << request_nbr << "…" << std::endl;
+        socket.send (request);
+
+        zmq::message_t reply;
+        socket.recv (&reply);
+        std::cout << "Received World " << request_nbr << std::endl;
+    }
+}
 
 	try {
 	}
